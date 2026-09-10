66020937ef625f667c60f451c853f174cf79ec0f
Min Kim <83806526+MK513@users.noreply.github.com>
2026-09-10 15:05:32 +0900

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 4.83 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
+    string answer = "Yes";
+    
+    int idx1 = 0, idx2 = 0;
+    for (string &g : goal) {
+        if (g == cards1[idx1]) {
+            idx1++;
+        }
+        else if (g == cards2[idx2]) {
+            idx2++;
+        }
+        else {
+            answer = "No";
+            break;
+        }
+    }
+    
+    
+    return answer;
+}
